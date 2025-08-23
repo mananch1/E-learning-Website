b5dc34c7f24ae23c0ce783414bf7a625c965581e
unknown <mananchahal01@gmail.com>
2025-08-23 21:34:51 +0530

    the next one

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     
     const isCourseListPage = location.pathname.includes('/course-list')
     return(
-      <div className={`flex item-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-grey-500 py-4 bg ${isCourseListPage?'bg-white':'bg-cyan-100/70'}`}>
+      <div className={`flex item-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-grey-500 py-4 bg ${isCourseListPage?'bg-white':'bg-cyan-q100/70'}`}>
         
         <img src={assets.logo} alt="Logo" className="w-28 lg:2-32 cursor-pointer" />
         <div className="hidden md:flex items-center gap-5 text-gray-500">
